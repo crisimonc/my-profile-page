d1f5776d8d620113b22922af5cf18da462315550
Cristina Simoncini <crisimonc@gmail.com>
2018-10-28 16:54:56 +0100

    ongoing

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,16 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+
+import Typed from 'typed.js';
+
+function loadDynamicBannerText() {
+  new Typed('#typed-text', {
+    strings: ["world trotter", "management engineer", "full stack developer"],
+    typeSpeed: 50,
+    loop: true
+  });
+}
+
+loadDynamicBannerText();
